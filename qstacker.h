86d0db4f7e4eb847cff8d0d44e41353a9d865b5f
Roy Bellingan <roy@tech.techadsmedia.com>
2021-10-19 12:47:40 +0100

    reformat

--- qstacker.h
+++ qstacker.h
@@ -1,39 +1,39 @@
 #ifndef QSTACKER_H
 #define QSTACKER_H
 
-#include <QByteArray>
 #include "exceptionv2.h"
+#include <QByteArray>
 
 struct QStackerOpt {
 	bool snippet = true;
 	bool object  = true;
 	bool address = true;
-	//prepend a newline
+	// prepend a newline
 	bool prependReturn = true;
 };
 constexpr QStackerOpt QStackerOptLight = {false, false, false};
 Q_REQUIRED_RESULT std::string stacker(uint skip = 3, QStackerOpt opt = QStackerOptLight);
-Q_REQUIRED_RESULT QByteArray QStacker(uint skip = 4, QStackerOpt opt = QStackerOptLight);
-Q_REQUIRED_RESULT QString QStacker16(uint skip = 4, QStackerOpt opt = QStackerOptLight);
-Q_REQUIRED_RESULT QString QStacker16Light(uint skip = 5, QStackerOpt opt = QStackerOptLight);
+Q_REQUIRED_RESULT QByteArray  QStacker(uint skip = 4, QStackerOpt opt = QStackerOptLight);
+Q_REQUIRED_RESULT QString     QStacker16(uint skip = 4, QStackerOpt opt = QStackerOptLight);
+Q_REQUIRED_RESULT QString     QStacker16Light(uint skip = 5, QStackerOpt opt = QStackerOptLight);
 /**
  * @brief StackerMinLevel is used to cut away the initial part of the stack trace
  * execially when running a thread there is a lot of stuff
  * It must be the name of the folder from which the code has been compiled
  */
 inline std::string StackerMinLevel;
-//Next throw will not append stack trace, reset after use
+// Next throw will not append stack trace, reset after use
 inline thread_local bool cxaNoStack = false;
 
 enum class CxaLevel {
-	//TODO in theory I should tweak ABORT, and always force print here the stacktrace generated inside __cxa
-	//lot of food for thought
-	none, //this will NOT forcefully print the stack on throw
+	// TODO in theory I should tweak ABORT, and always force print here the stacktrace generated inside __cxa
+	// lot of food for thought
+	none, // this will NOT forcefully print the stack on throw
 	warn,
 	debug,
 	critical
 };
-//what level shall we use ? reset after use as critical
+// what level shall we use ? reset after use as critical
 inline thread_local CxaLevel cxaLevel = CxaLevel::critical;
 
 void messanger(const QString& msg, CxaLevel level);
